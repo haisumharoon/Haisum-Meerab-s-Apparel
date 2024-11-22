0103806ae28f738ae33c976fbd7176ce5f1e9ae9
haisum2 <haisum921@gmail.com>
2024-11-21 17:11:34 -0800

    Adds dropdown component to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Navbar.css"; // We'll create this CSS file for styling
 import Searchbar from "./Searchbar/Searchbar";
 import Categories from "./Categories/Categories";
+import Dropdown from "./Dropdown/Dropdown";
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn }) => {
+  const [showDD, setShowDD] = useState(false);
   const navbar_ref = useRef();
   let prevScrollPos = window.pageYOffset; // Store the initial scroll position
 
@@ -16,7 +18,7 @@ const Navbar = () => {
       navbar.style.top = "0";
     } else {
       // Scrolling down, hide navbar
-      navbar.style.top = "-80px"; // Adjust this value based on your navbar's height
+      navbar.style.top = "-80px";
     }
 
     prevScrollPos = currentScrollPos; // Update the previous scroll position
@@ -42,20 +44,63 @@ const Navbar = () => {
           </li>
           <li></li>
           <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              fill="#fff"
-              class="bi bi-person-circle"
-              viewBox="0 0 16 16"
+            <a
+              href={isLoggedIn ? "#" : "./login"}
+              onMouseEnter={() => {
+                setShowDD(true);
+              }}
+              onMouseLeave={() => {
+                setShowDD(false);
+              }}
             >
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-              <path
-                fillRule="evenodd"
-                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="#fff"
+                class="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path
+                  fillRule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                />
+              </svg>
+            </a>
+
+            <Dropdown
+              options={[
+                {
+                  title: "Log out",
+                  svg: (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-box-arrow-right"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                      />
+                    </svg>
+                  ),
+                  clickListener: () => {
+                    localStorage.removeItem("jwtToken");
+                    window.location.href = "/login";
+                  },
+                },
+              ]}
+              showDD={showDD}
+              setShowDD={setShowDD}
+            />
           </li>
         </ul>
       </div>
