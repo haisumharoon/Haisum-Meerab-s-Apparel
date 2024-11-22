b6e2224a03068234437398acfd56d3aee330fb65
haisum2 <haisum921@gmail.com>
2024-11-21 17:07:05 -0800

    Creates CRUD endpoints for categories

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const path = require('path');
 const mysql = require('mysql'); 
 const createTables = require("./tables")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const app = express();
@@ -51,9 +52,147 @@ app.get('/uploads/:filename', (req, res) => {
         }
     });
 });
-app.get("/category",(req, res) => {
-    return res.status(200).json({message:"This is the categories route"})
+app.get("/user",authorize_token,(req,res)=>{ 
+    con.query(`SELECT * FROM user WHERE user_id = "${req.user.id}";`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({user:result[0]})
+    });
+})
+app.get("/categories",(req,res)=>{
+    const start = new Date().getTime()
+    con.query(`SELECT * FROM customer_category;`, (err, result)=> {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+         result.forEach((category,index)=>{
+             con.query(`SELECT * FROM categories WHERE cc_id = ${category.cc_id};`,  function (err, subCategories) {
+                if(err){
+                    return res.status(500).json({message:""+err})
+                } 
+                subCategories.forEach((subCategory,i)=>{
+                    con.query(`SELECT * FROM sub_categories WHERE category_id = ${subCategory.category_id};`, function (err, subSubCategories) {
+                        if(err){
+                            return res.status(500).json({message:""+err})
+                        }
+                        subCategory.subCategories = subSubCategories
+                        if(i == subCategories.length-1 && index == result.length-1){
+                            return res.status(200).json({categories:result})
+                        }
+                    })
+                })
+                category.subCategories = subCategories
+            });
+        })  
+    });
+})
+app.post("/customer-categories",authorize_token,(req,res)=>{
+    const {name} = req.body 
+    con.query(`INSERT INTO customer_category (name) VALUES ("${name}");`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category created successfully"})
+    });
+})
+app.put("/customer-categories/:id",authorize_token,(req,res)=>{
+    const {name} = req.body 
+    con.query(`UPDATE customer_category SET name = "${name}" WHERE cc_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category updated successfully"})
+    });
+})
+app.delete("/customer-categories/:id",authorize_token,(req,res)=>{
+    con.query(`DELETE FROM customer_category WHERE cc_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category deleted successfully"})
+    });
+})
+app.get("/customer-categories",(req,res)=>{
+    con.query(`SELECT * FROM customer_category;`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({categories:result})
+    });
+})
+app.get("/categories",(req,res)=>{
+    con.query(`SELECT * FROM categories;`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({categories:result})
+    });
+})
+app.get("/sub-categories",(req,res)=>{
+    con.query(`SELECT * FROM sub_categories;`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({categories:result})
+    });
+})
+app.post("/categories",authorize_token,(req,res)=>{
+    const {cc_id,name} = req.body 
+    con.query(`INSERT INTO categories (cc_id,name) VALUES ("${cc_id}","${name}");`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category created successfully"})
+    });
+})
+app.put("/categories/:id",authorize_token,(req,res)=>{
+    const {name} = req.body 
+    con.query(`UPDATE categories SET name = "${name}" WHERE category_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category updated successfully"})
+    });
 });
+app.delete("/categories/:id",authorize_token,(req,res)=>{
+    con.query(`DELETE FROM categories WHERE category_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category deleted successfully"})
+    });
+})
+app.post("/sub-categories",authorize_token,(req,res)=>{
+    const {category_id,name} = req.body 
+    con.query(`INSERT INTO sub_categories (category_id,name) VALUES ("${category_id}","${name}");`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category created successfully"})
+    });
+}
+)
+app.put("/sub-categories/:id",authorize_token,(req,res)=>{
+    const {name} = req.body 
+    con.query(`UPDATE sub_categories SET name = "${name}" WHERE sub_category_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category updated successfully"})
+    });
+}
+)
+app.delete("/sub-categories/:id",authorize_token,(req,res)=>{
+    con.query(`DELETE FROM sub_categories WHERE sub_category_id = ${req.params.id};`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        return res.status(200).json({message:"Category deleted successfully"})
+    });
+}
+)
+
 app.post("/register",(req,res)=>{
     const {name,username,email,password} = req.body 
     con.query(`INSERT INTO credentials (username,email,password) VALUES ("${username}","${email}","${password}");`, function (err, result) {
@@ -69,5 +208,43 @@ app.post("/register",(req,res)=>{
         return res.status(200).json({message:"User registered successfully"})
     });
     });
-    
 })
+
+app.post("/login", (req,res)=>{
+    const {email,password} = req.body 
+    con.query(`SELECT * FROM credentials WHERE email = "${email}" AND password = "${password}";`, function (err, result) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        if(result.length == 0){
+            return res.status(401).json({message:"Invalid credentials"})
+        }
+        con.query(`SELECT * FROM user WHERE credential_id = ${result[0].credential_id};`, function (err, userResult) {
+        if(err){
+            return res.status(500).json({message:""+err})
+        }
+        
+        const token =  jwt.sign({ sub: 'userId' , id: userResult[0].user_id},process.env.ACCESSTOKEN)
+        console.log(token);
+        return res.status(200).json({message:"User logged in successfully",token:token})
+    }
+    );
+    });
+});
+
+function authorize_token(req,res,next){
+    const auth_tkn = req.headers["authorization"]
+    const token = auth_tkn?.split(" ")[1];
+    if(token == null){
+        res.status(401).json({message:"Bad request, no auth"})
+    }
+    var decoded;
+    try{
+    decoded = jwt.verify(token,process.env.ACCESSTOKEN);
+}catch(e){
+    
+            return res.status(401).send('unauthorized');
+}
+    req.user = decoded 
+    next()
+}
