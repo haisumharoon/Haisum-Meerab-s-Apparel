0aa4aee7c16f84e3104662aead1b580a32e9d969
haisum2 <haisum921@gmail.com>
2024-12-01 17:25:01 -0800

    Enhances CategoryList component with hover effects and click functionality for subcategories

--- src/components/NavBar/Categories/Subcategories/CategoryList/CategoryList.jsx
+++ src/components/NavBar/Categories/Subcategories/CategoryList/CategoryList.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+import UserContext from "../../../../../Context/Context";
 import "./CategoryList.css";
 const CategoryList = ({ sub_cat }) => {
+  const { selectedCategory, setSelectedCategory } = useContext(UserContext);
   return (
     <div className="list-container">
       <div className="cat_name">{sub_cat?.name}</div>
       <ul className="subcats">
         {sub_cat?.sub_categories?.map((subsubsub) => (
-          <li>{subsubsub.name}</li>
+          <li
+            className="subcat"
+            onClick={() => {
+              setSelectedCategory(subsubsub.sub_category_id);
+            }}
+          >
+            {subsubsub.name}
+          </li>
         ))}
       </ul>
     </div>
