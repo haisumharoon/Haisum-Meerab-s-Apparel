239e1a0af4976182aee7bb52c369d871bfdd1c1a
haisum2 <haisum921@gmail.com>
2024-12-01 17:24:40 -0800

    Updates Navbar component to replace cart icon with home and wishlist icons, enhancing navigation options

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -31,28 +31,36 @@ const Navbar = ({ isLoggedIn }) => {
         </div>
         <ul className="nav-links">
           <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              fill="#fff"
-              class="bi bi-cart-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-            </svg>
+            <a href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="currentColor"
+                class="bi bi-house-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+              </svg>
+            </a>
           </li>
-          <li></li>
           <li>
-            <a
-              href={isLoggedIn ? "#" : "./login"}
-              onMouseEnter={() => {
-                setShowDD(true);
-              }}
-              onMouseLeave={() => {
-                setShowDD(false);
-              }}
-            >
+            <a href="/wishlist">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="currentColor"
+                class="bi bi-bag-heart-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href={isLoggedIn ? "#" : "./login"}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="26"
@@ -68,39 +76,6 @@ const Navbar = ({ isLoggedIn }) => {
                 />
               </svg>
             </a>
-
-            <Dropdown
-              options={[
-                {
-                  title: "Log out",
-                  svg: (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-box-arrow-right"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-                      />
-                      <path
-                        fill-rule="evenodd"
-                        d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-                      />
-                    </svg>
-                  ),
-                  clickListener: () => {
-                    localStorage.removeItem("jwtToken");
-                    window.location.href = "/login";
-                  },
-                },
-              ]}
-              showDD={showDD}
-              setShowDD={setShowDD}
-            />
           </li>
         </ul>
       </div>
