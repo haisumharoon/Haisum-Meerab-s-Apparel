30ffa817591f974ae66be87f3dbbbae0ee1230d5
haisum2 <haisum921@gmail.com>
2024-12-02 19:41:54 -0800

    Adds orders button and styles for orders container in Admin component

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -32,83 +32,88 @@ const Admin = () => {
     verifyUser();
   }, []);
   return (
-    <div className="models">
-      <TableObjects
-        table_title={"Customer Categories"}
-        url={"http://localhost:5000/customer-categories"}
-        attributes={[{ name: "name", type: "text" }]}
-        defining_property={"name"}
-        id_name={"cc_id"}
-      />
-      <TableObjects
-        table_title={"Categories"}
-        url={"http://localhost:5000/categories"}
-        attributes={[
-          { name: "name", type: "text" },
-          {
-            name: "cc_id",
-            type: "select",
-            url: "http://localhost:5000/customer-categories",
-            defining_prop: "name",
-          },
-        ]}
-        defining_property={"name"}
-        id_name={"category_id"}
-        description_property={"parent_description"}
-      />
-      <TableObjects
-        table_title={"Sub categories"}
-        url={"http://localhost:5000/sub-categories"}
-        description_property={"parent_description"}
-        attributes={[
-          { name: "name", type: "text" },
-          {
-            name: "category_id",
-            type: "select",
-            url: "http://localhost:5000/categories",
-            defining_prop: "name",
-            descriptive_prop: "parent_description",
-          },
-        ]}
-        defining_property={"name"}
-        id_name={"sub_category_id"}
-      />
-      <TableObjects
-        table_title={"Users"}
-        description_property={"username"}
-        url={"http://localhost:5000/users"}
-        attributes={[
-          { name: "name", type: "text" },
-          { name: "email", type: "email" },
-          { name: "password", type: "text" },
-          { name: "username", type: "text" },
-          { name: "isAdmin", type: "checkbox" },
-        ]}
-        defining_property={"name"}
-        id_name={"credential_id"}
-      />
-      <TableObjects
-        table_title={"Listings"}
-        // description_property={""}
-        url={"http://localhost:5000/admin/listing"}
-        attributes={[
-          { name: "title", type: "text" },
-          { name: "stock", type: "number" },
-          { name: "price", type: "text" },
-          { name: "file", type: "file" },
-          {
-            name: "sub_categories",
-            type: "multiple",
-            url: "http://localhost:5000/sub-categories",
-            defining_prop: "name",
-            id_prop: "sub_category_id",
-            descriptive_prop: "parent_description",
-          },
-        ]}
-        defining_property={"title"}
-        id_name={"listing_id"}
-      />
-    </div>
+    <>
+      <div className="models">
+        <TableObjects
+          table_title={"Customer Categories"}
+          url={"http://localhost:5000/customer-categories"}
+          attributes={[{ name: "name", type: "text" }]}
+          defining_property={"name"}
+          id_name={"cc_id"}
+        />
+        <TableObjects
+          table_title={"Categories"}
+          url={"http://localhost:5000/categories"}
+          attributes={[
+            { name: "name", type: "text" },
+            {
+              name: "cc_id",
+              type: "select",
+              url: "http://localhost:5000/customer-categories",
+              defining_prop: "name",
+            },
+          ]}
+          defining_property={"name"}
+          id_name={"category_id"}
+          description_property={"parent_description"}
+        />
+        <TableObjects
+          table_title={"Sub categories"}
+          url={"http://localhost:5000/sub-categories"}
+          description_property={"parent_description"}
+          attributes={[
+            { name: "name", type: "text" },
+            {
+              name: "category_id",
+              type: "select",
+              url: "http://localhost:5000/categories",
+              defining_prop: "name",
+              descriptive_prop: "parent_description",
+            },
+          ]}
+          defining_property={"name"}
+          id_name={"sub_category_id"}
+        />
+        <TableObjects
+          table_title={"Users"}
+          description_property={"username"}
+          url={"http://localhost:5000/users"}
+          attributes={[
+            { name: "name", type: "text" },
+            { name: "email", type: "email" },
+            { name: "password", type: "text" },
+            { name: "username", type: "text" },
+            { name: "isAdmin", type: "checkbox" },
+          ]}
+          defining_property={"name"}
+          id_name={"credential_id"}
+        />
+        <TableObjects
+          table_title={"Listings"}
+          // description_property={""}
+          url={"http://localhost:5000/admin/listing"}
+          attributes={[
+            { name: "title", type: "text" },
+            { name: "stock", type: "number" },
+            { name: "price", type: "text" },
+            { name: "file", type: "file" },
+            {
+              name: "sub_categories",
+              type: "multiple",
+              url: "http://localhost:5000/sub-categories",
+              defining_prop: "name",
+              id_prop: "sub_category_id",
+              descriptive_prop: "parent_description",
+            },
+          ]}
+          defining_property={"title"}
+          id_name={"listing_id"}
+        />
+      </div>
+      <div className="orders-container">
+        <button className="orders">View Orders</button>
+      </div>
+    </>
   );
 };
 
